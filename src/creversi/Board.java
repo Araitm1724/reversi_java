411e1321fbf4caa2315e658b17349966544cfd0f
荒井智之進 <z3536294@gmail.com>
2025-04-28 17:32:58 +0900

    add comments.

--- src/creversi/Board.java
+++ src/creversi/Board.java
@@ -4,36 +4,48 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * リバーシ盤クラス
+ */
 public class Board {
-	int[][] boardStatus = new int[10][10];
-	final int SPACE = 0, BLACK = 1, WHITE = BLACK * -1, WALL = 9;
+	int[][] boardStatus = new int[10][10]; // 盤の状態（外側（[0]と[9]）は番兵）
+
+	final int BLACK = 1, WHITE = BLACK * -1, SPACE = 0, WALL = 9; // マスの状態を表す
+
+	char[] squareStatus = { '　', '●', '○' }; // 盤の状態（マスの描画に使用）
 
 	final int[][] value = { { 45, -11, 4, -1, -1, 4, -11, 45 }, { -11, -16, 1, -3, -3, -1, -16, -11 },
 			{ 4, -1, 2, -1, -1, 2, -1, 4 }, { -1, -3, -1, 0, 0, -1, -3, -1 }, { -1, -3, -1, 0, 0, -1, -3, -1 },
 			{ 4, -1, 2, -1, -1, 2, -1, 4 }, { -11, -16, 1, -3, -3, -1, -16, -11 },
-			{ 45, -11, 4, -1, -1, 4, -11, 45 }, };
+			{ 45, -11, 4, -1, -1, 4, -11, 45 }, }; // マスの評価値（COMレベル2が使用）
 
-	char[] squareStatus = { '　', '●', '○' };
+	int blackCount, whiteCount; // それぞれの石の数
 
-	int blackCount, whiteCount;
+	int y, x, nextY, nextX, furtherY, furtherX, previousY, previousX; // マスを調べる際のループ用
 
-	int[] coordinate = new int[2];
-	int y, x, nextY, nextX, furtherY, furtherX, previousY, previousX;
-	List<int[]> movable = new ArrayList<int[]>();
+	int[] coordinate = new int[2]; // 石を打てるマスの座標
 
+	List<int[]> movable = new ArrayList<int[]>(); // 石を打てるマスの座標一覧
+
+	/**
+	 * 対局開始時に盤の状態をリセットする
+	 */
 	void resetBoard() {
-		for (y = 0; y < boardStatus.length; y++) {
+		for (y = 0; y < boardStatus.length; y++) { // 外側を壁（番兵）、それ以外を全て空白にする
 			for (x = 0; x < boardStatus[0].length; x++) {
 				boardStatus[y][x] = (y == 0 || x == 0 || y == 10 || x == 10) ? WALL : SPACE;
 			}
 		}
 
-		boardStatus[4][4] = BLACK;
-		boardStatus[5][5] = BLACK;
-		boardStatus[4][5] = WHITE;
-		boardStatus[5][4] = WHITE;
+		boardStatus[4][5] = BLACK; // 中央に石を配置
+		boardStatus[5][4] = BLACK;
+		boardStatus[4][4] = WHITE;
+		boardStatus[5][5] = WHITE;
 	}
 
+	/**
+	 * 現在の盤の状態を描画する
+	 */
 	void drawBoard() {
 		System.out.println("  １ ２ ３ ４ ５ ６ ７ ８");
 
@@ -55,9 +67,12 @@ public class Board {
 
 		System.out.println(" +ー+ー+ー+ー+ー+ー+ー+ー+");
 
-		countStones();
+		countStones(); // 描画を終えたら石を数える
 	}
 
+	/**
+	 * 石を数えて表示する
+	 */
 	void countStones() {
 		blackCount = 0;
 		whiteCount = 0;
@@ -75,28 +90,33 @@ public class Board {
 		System.out.println("黒：" + blackCount + " 白：" + whiteCount);
 	}
 
+	/**
+	 * 打てるマスを抽出する
+	 * 
+	 * @param currentTurn 現在の手番（石の色）
+	 */
 	void checkSquares(int currentTurn) {
 		movable.clear();
 
 		for (y = 1; y <= 8; y++) {
 			for (x = 1; x <= 8; x++) {
-				if (boardStatus[y][x] == SPACE) {
+				if (boardStatus[y][x] == SPACE) { // 空白マスを探す
 					for (nextY = -1; nextY <= 1; nextY++) {
 						for (nextX = -1; nextX <= 1; nextX++) {
-							if (boardStatus[y + nextY][x + nextX] == currentTurn * -1) {
+							if (boardStatus[y + nextY][x + nextX] == currentTurn * -1) { // 空白マスの隣が相手の石か調べる
 								furtherY = (y + nextY);
 								furtherX = (x + nextX);
 
-								while (true) {
-									furtherY += nextY;
+								while (true) { // 相手の石の更に先を調べる
+									furtherY += nextY; // 同じ方向の更に先に進む
 									furtherX += nextX;
 
-									if (boardStatus[furtherY][furtherX] == currentTurn) {
-										coordinate[0] = y;
+									if (boardStatus[furtherY][furtherX] == currentTurn) { // 自分の石か調べる
+										coordinate[0] = y; // 起点の空白マスを有効マスとして登録
 										coordinate[1] = x;
 										movable.add(Arrays.copyOf(coordinate, coordinate.length));
 									} else if (boardStatus[furtherY][furtherX] == SPACE
-											|| boardStatus[furtherY][furtherX] == WALL) {
+											|| boardStatus[furtherY][furtherX] == WALL) { // 空白マスか壁に当たればその方向はハズレ
 										break;
 									}
 								}
@@ -108,28 +128,35 @@ public class Board {
 		}
 	}
 
+	/**
+	 * 石を反転させる
+	 * 
+	 * @param moveSquare 石を打ったマスの座標
+	 * @param currentTurn 現在の手番（石の色）
+	 */
 	void reverseStone(int[] moveSquare, int currentTurn) {
 		for (nextY = -1; nextY <= 1; nextY++) {
 			for (nextX = -1; nextX <= 1; nextX++) {
-				if (boardStatus[moveSquare[0] + nextY][moveSquare[1] + nextX] == currentTurn * -1) {
+				if (boardStatus[moveSquare[0] + nextY][moveSquare[1] + nextX] == currentTurn * -1) { // 打ったマスの隣が相手の石か調べる
 					furtherY = (moveSquare[0] + nextY);
 					furtherX = (moveSquare[1] + nextX);
 
-					while (true) {
+					while (true) { // 相手の石の更に先を調べる
 						furtherY += nextY;
 						furtherX += nextX;
 
-						if (boardStatus[furtherY][furtherX] == currentTurn) {
+						if (boardStatus[furtherY][furtherX] == currentTurn) { // 自分の石か調べる
 							previousY = furtherY;
 							previousX = furtherX;
 
-							do {
-								previousY += nextY * -1;
+							do { // 相手の石を自分に石に変えていく
+								previousY += nextY * -1; // 来た方向を逆戻りする
 								previousX += nextX * -1;
+
 								boardStatus[previousY][previousX] = currentTurn;
 							} while (previousY != moveSquare[0] || previousX != moveSquare[1]);
 						} else if (boardStatus[furtherY][furtherX] == SPACE
-								|| boardStatus[furtherY][furtherX] == WALL) {
+								|| boardStatus[furtherY][furtherX] == WALL) { // 空白マスか壁に当たればその方向はハズレ
 							break;
 						}
 					}
--- src/creversi/Game.java
+++ src/creversi/Game.java
@@ -6,33 +6,36 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Game {
-	int i;
+	int loop;
 	int playerStone, enemyStone;
 
 	int enemyLevel;
 
-	int currentTurn;
+	int currentTurn; // 現在の手番（石の色）
 
-	int turns;
+	int turns; // 総手数
 
-	int[] moveSquare = new int[2];
+	int[] moveSquare = new int[2]; // プレイヤーが打った（入力した）マスの座標
 
-	int passCount;
+	int passCount; // どこにも打てないときに加算する（2以上になったら終局）
 
-	int maxValue;
+	int maxValue; // 最も評価値の高い有効マスの座標（COMレベル2が使用）
 
-	boolean isThinking;
+	boolean isThinking; // プレイヤーの入力中
 
-	int choice;
 	BufferedReader playerInput = new BufferedReader(new InputStreamReader(System.in));
+	int choice; // プレイヤーが入力した値
 
 	Board board = new Board();
 
 	final int WIN = 1, LOSE = -1, DRAW = 0;
 
-	boolean start() throws IOException {
-		System.out.println("ようこそ\nここではリバーシをプレイできます。");
-
+	/**
+	 * 対局設定の選択
+	 * 
+	 * @throws IOException
+	 */
+	void start() throws IOException {
 		do {
 			System.out.println("石の色を選んで下さい。");
 			System.out.print("黒（先手）：" + board.BLACK + "、白（後手）：" + board.WHITE + ">");
@@ -46,53 +49,57 @@ public class Game {
 			System.out.print("レベル1：1、レベル2：2>");
 			enemyLevel = inputNumber();
 		} while (enemyLevel != 1 && enemyLevel != 2);
-
-		return true;
 	}
 
+	/**
+	 * 対局
+	 * 
+	 * @return 勝敗
+	 * @throws IOException
+	 */
 	int playGame() throws IOException {
 		System.out.println("対局開始");
-		board.resetBoard();
 
+		board.resetBoard();
 		currentTurn = 1;
 		turns = 0;
 		passCount = 0;
 
-		while (turns < 60 && passCount < 2) {
+		while (turns < 60 && passCount < 2) { // 打てるマスが無くなるまで対局を続ける
 			board.drawBoard();
 			board.checkSquares(currentTurn);
 
-			if (board.movable.size() > 0) {
-				passCount = 0;
+			if (board.movable.size() > 0) { // 打てるマスがあれば手番を回す
+				passCount = 0; // 連続パス回数をリセット
 
 				if (currentTurn == playerStone) {
-					isThinking = true;
-
 					System.out.println("貴方の番です。");
 					System.out.println("打ちたいマスの座標を順番に入力して下さい。");
 					System.out.println("投了（降参）したい場合は-1を入力して下さい。");
 
-					do {
-						for (i = 0; i < moveSquare.length; i++) {
-							do {
-								System.out.print((i == 0) ? "行（Y座標）>" : "列（X座標）>");
-								moveSquare[i] = inputNumber();
+					isThinking = true;
 
-								if (moveSquare[i] == -1) {
+					do { // 
+						for (loop = 0; loop < moveSquare.length; loop++) { // Y座標とX座標の2回入力させる
+							do {
+								System.out.print((loop == 0) ? "行（Y座標）>" : "列（X座標）>");
+								moveSquare[loop] = inputNumber();
+
+								if (moveSquare[loop] == -1) {
 									System.out.print("投了しますか?\nYES：1、NO：-1>");
 
 									if (inputNumber() == 1) {
-										return LOSE;
+										return LOSE; // 投了したらそこで終了
 									}
-								} else if (!(moveSquare[i] >= 1 && moveSquare[i] <= 8)) {
+								} else if (!(moveSquare[loop] >= 1 && moveSquare[loop] <= 8)) {
 									System.out.println("範囲外の値です。");
 								}
-							} while (moveSquare[i] < 1 || moveSquare[i] > 8);
+							} while (moveSquare[loop] < 1 || moveSquare[loop] > 8);
 						}
 
-						for (i = 0; i < board.movable.size(); i++) {
-							if (Arrays.equals(moveSquare, board.movable.get(i))) {
-								board.reverseStone(moveSquare, playerStone);
+						for (loop = 0; loop < board.movable.size(); loop++) { // 有効マスに入力したマスが含まれているか
+							if (Arrays.equals(moveSquare, board.movable.get(loop))) {
+								board.reverseStone(moveSquare, playerStone); // 石を打って手番終了
 								isThinking = false;
 								break;
 							}
@@ -106,7 +113,7 @@ public class Game {
 					System.out.println("相手の番です。");
 					board.reverseStone((enemyLevel == 1) ? level1() : level2(), enemyStone);
 
-					try {
+					try { // 2秒待つ（これが無いと一瞬で終わるので混乱する）
 						Thread.sleep(2000);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
@@ -118,41 +125,63 @@ public class Game {
 				System.out.println((++passCount < 2) ? "打てる手が無いのでパスします。" : "お互いに打てる手が無くなりました。");
 			}
 
-			currentTurn *= -1;
+			currentTurn *= -1; // 相手に手番を回す
 		}
 
-		return gameOver();
+		return gameOver(); // 対局が終了したら結果の判定
 	}
 
-	int[] level1() {
+	/**
+	 * COMレベル1の思考
+	 * 
+	 * @return 石を打つマスの座標
+	 */
+	int[] level1() { // 有効マスの中からランダムに打つ
 		return board.movable.get((int) (Math.random() * board.movable.size()));
 	}
 
+	/**
+	 * COMレベル2の思考
+	 * 
+	 * @return 石を打つマスの座標
+	 */
 	int[] level2() {
-		maxValue = 0;
+		maxValue = 0; // 最も評価値の高い有効マスの座標（まず1番目を代入）
 
-		for (i = 1; i < board.movable.size(); i++) {
-			maxValue = (board.value[board.movable.get(i)[0] - 1][board.movable.get(i)[1]
-					- 1] > board.value[board.movable.get(maxValue)[0] - 1][board.movable.get(maxValue)[1] - 1]) ? i
-							: maxValue;
+		for (loop = 1; loop < board.movable.size(); loop++) { // 2番目から繰り返し始める
+			if (board.value[board.movable.get(loop)[0] - 1][board.movable.get(loop)[1]
+					- 1] > board.value[board.movable.get(maxValue)[0] - 1][board.movable.get(maxValue)[1] - 1]) {
+				maxValue = loop; // 現在のマスが前のマスより評価値が高ければ更新
+			}
 		}
 
 		return board.movable.get(maxValue);
 	}
 
+	/**
+	 * 対局結果の判定
+	 * 
+	 * @return 勝敗
+	 */
 	int gameOver() {
 		System.out.println("終局");
 		board.drawBoard();
 
-		if (board.blackCount > board.whiteCount) {
+		if (board.blackCount > board.whiteCount) { // 黒の方が多く、プレイヤーが黒なら勝ち、白なら負け
 			return (playerStone == board.BLACK ? WIN : LOSE);
-		} else if (board.blackCount < board.whiteCount) {
+		} else if (board.blackCount < board.whiteCount) { // 白の方が多く、プレイヤーが白なら勝ち、黒なら負け
 			return (playerStone == board.WHITE ? WIN : LOSE);
-		} else {
+		} else { //白黒同数なら引き分け
 			return DRAW;
 		}
 	}
 
+	/**
+	 * プレイヤーのコマンド選択と着手
+	 * 
+	 * @return 入力した数値
+	 * @throws IOException
+	 */
 	int inputNumber() throws IOException {
 		choice = 0;
 
