31d24d5bfd64e8ef8e39bc2d19f271ae4b836701
alba5116 <dumspirospero_1060103@aol.com>
2025-05-06 17:59:03 +0900

    created panels.

--- src/reversi/greversi/Window.java
+++ src/reversi/greversi/Window.java
@@ -13,45 +13,35 @@ public class Window extends JFrame {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int windowWidth = (int) (screenSize.getWidth() * 0.8);
 		int windowHeight = (int) (screenSize.getHeight() * 0.8);
+		int windowStartX = (int) (screenSize.getWidth() * 0.05);
+		int windowStartY = (int) (screenSize.getHeight() * 0.05);
+		int fontSize = (int) (windowWidth * 0.025);
+		System.out.println(windowWidth);
+		System.out.println(windowHeight);
 
 		// メインフレームの設定
 		setTitle("Reversi");
-		setBounds(50, 50, windowWidth, windowHeight);
+		setBounds(windowStartX, windowStartY, windowWidth, windowHeight);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		// 先後選択ラジオボタンの設定
-		JLabel colorLabel = new JLabel("石の色を選択");
-		JRadioButton blackButton = new JRadioButton("黒（先手）", true);
-		JRadioButton whiteButton = new JRadioButton("白（後手）");
+		// メインパネル
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new GridLayout());
 
-		// 2つのボタンを同じグループにまとめる
-		ButtonGroup colorButtons = new ButtonGroup();
-		colorButtons.add(blackButton);
-		colorButtons.add(whiteButton);
+		// 盤パネル
+		JPanel boardPanel = new JPanel();
+		mainPanel.add(boardPanel);
 
-		JPanel selectColor = new JPanel();
-		selectColor.add(colorLabel);
-		selectColor.add(blackButton);
-		selectColor.add(whiteButton);
+		// メニューパネル
+		JPanel menuPanel = new JPanel();
+		mainPanel.add(menuPanel);
 
-		Container contentPane = getContentPane();
-		contentPane.add(selectColor, BorderLayout.EAST);
+		// 対局情報パネル
+		JPanel gamePanel = new JPanel();
 
-		// COMレベル選択ラジオボタンの設定
-		JLabel levelLabel = new JLabel("COMのレベルを選択");
-		JRadioButton level1Button = new JRadioButton("レベル1", true);
-		JRadioButton level2Button = new JRadioButton("レベル2", true);
+		// 終局パネル
+		JPanel endPanel = new JPanel();
 
-		// 2つのボタンを同じグループにまとめる
-		ButtonGroup levelButtons = new ButtonGroup();
-		levelButtons.add(level1Button);
-		levelButtons.add(level2Button);
-
-		JPanel selectLevel = new JPanel();
-		selectLevel.add(levelLabel);
-		selectLevel.add(level1Button);
-		selectLevel.add(level2Button);
-
-		contentPane.add(selectLevel, BorderLayout.EAST);
+		getContentPane().add(mainPanel, BorderLayout.CENTER);
 	}
 }
